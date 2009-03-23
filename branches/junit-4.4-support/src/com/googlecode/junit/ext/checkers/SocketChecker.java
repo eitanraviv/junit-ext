332588eb9527d815cf22560d0ea3ce91f0ed7d5c
Iamkaihu <Iamkaihu@5290d482-a5a5-11dd-bf8d-3712e0689f7b>
2009-03-23 14:38:31 +0000

    Added the timeout into the SocketChecker and HttpChecker

--- branches/junit-4.4-support/src/com/googlecode/junit/ext/checkers/SocketChecker.java
+++ branches/junit-4.4-support/src/com/googlecode/junit/ext/checkers/SocketChecker.java
@@ -1,6 +1,7 @@
 package com.googlecode.junit.ext.checkers;
 
 import java.net.Socket;
+import java.net.InetSocketAddress;
 
 public class SocketChecker implements Checker {
     private String host;
@@ -18,7 +19,9 @@ public class SocketChecker implements Checker {
     public boolean satisfy() {
         Socket socket = null;
         try {
-            socket = new Socket(host, port);
+            Socket sock = new Socket();
+            sock.bind(null);
+            sock.connect(new InetSocketAddress(host, port), 10000);
             return true;
         } catch (Exception e) {
             return false;
