86ece2e00cf384f3f805211226f7d7040eafe064
Iamkaihu <Iamkaihu@5290d482-a5a5-11dd-bf8d-3712e0689f7b>
2009-03-24 07:02:35 +0000

    compile with JDK5 and added the class level ignoring.

--- branches/junit-4.4-support/src/com/googlecode/junit/ext/RunIf.java
+++ branches/junit-4.4-support/src/com/googlecode/junit/ext/RunIf.java
@@ -2,10 +2,14 @@ package com.googlecode.junit.ext;
 
 import com.googlecode.junit.ext.checkers.Checker;
 
-@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
-@java.lang.annotation.Target(value = {java.lang.annotation.ElementType.METHOD})
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@java.lang.annotation.Retention(value = RUNTIME)
+@java.lang.annotation.Target(value = {METHOD, TYPE})
 public @interface RunIf {
-    Class<? extends Checker> value() ;
+    Class<? extends Checker> value();
 
     String[] arguments() default {};
 }
